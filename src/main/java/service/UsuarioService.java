8f2c2bdb808a983ac4705ec32aaad732219055ee
Guilherme Januário <guilherme.januario@zup.com.br>
2023-07-12 17:15:38 -0300

    Update UsuarioService.java
    :rocket: - Subindo conexão com o banco"

--- src/main/java/service/UsuarioService.java
+++ src/main/java/service/UsuarioService.java
@@ -3,12 +3,27 @@ package service;
 import connection.Conexao;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class UsuarioService {
 
     Connection connection = Conexao.getConnection();
 
 
+    public void listandoTodosUsuario(){
+
+        try {
+            if (connection != null) {
+
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
 
 
 
